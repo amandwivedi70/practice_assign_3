6e71da5834c6c08a951e67cdcffc8c953a2b0cc1
amandwivedi70 <111753149+amandwivedi70@users.noreply.github.com>
2022-09-11 14:39:33 +0530

    Add files via upload

---
+++ practice_assign_3.py
@@ -0,0 +1,48 @@
+# question 1
+
+x = 5
+y = "10"
+
+a = str(x)
+print(type(a))
+
+# question 2
+
+x = "m"
+ord(x)
+
+# question 3
+b = 100
+print(chr(100))
+
+# question 4
+x = 10
+print(bin(x))
+
+# question 5
+x = 10
+print(oct(x))
+
+# question 6
+x = 10
+print(hex(x))
+
+# question 7
+
+a = 0b1100101
+print(a)
+
+# question 8
+
+x = 0x2f
+print(oct(x))
+
+# question 9
+x = 0o125
+print(bin(x))
+
+# question 10
+
+x = 0o25
+y = 0x39
+print(bin(x+y))
